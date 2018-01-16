4533ea0adb1ebfb9a2de2c77dc93e8d08101700b
Maria Burlando <maria.bu62442@gmail.com>
2018-01-16 20:21:13 +0100

    Created dark theme for card

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
 import LightGithub from './indexlight';
-//import DarkGithub from './indexdark'
+import DarkGithub from './indexdark'
 
 
 class App extends Component {
 
   render() {
     return(
+      <div>
       <LightGithub user = "Mary62442"/>
+      <DarkGithub user = "diegomary"/>
+      </div>
       )
   }
   
--- src/indexdark.js
+++ src/indexdark.js
@@ -23,25 +23,22 @@ class DarkGithub extends Component {
 	}
 
 	render(){
-
-		
-
 		return(
-			<div className = {styles.container}>
+			<div className = "containerDark">
 			    <a href={this.state.data.html_url}>
-			    <div className = {styles.flame}>
-					<img alt="" className = {styles.avatar} src={this.state.data.avatar_url} />
+			    <div className = "flameDark">
+					<img alt="" className = "avatarDark" src={this.state.data.avatar_url} />
 					<div></div>
 				</div>
 				</a>
 				<hr/>
-				<a className = {styles.username} href={this.state.data.html_url}>
+				<a className = "usernameDark" href={this.state.data.html_url}>
 					<p>
 						<i className="fa fa-github" aria-hidden="true"></i>
 					{this.state.data.login}</p>
 				</a>
-				<p className = {styles.location}>{this.state.data.location}</p>
-				<div className = {styles.info}>
+				<p className = "locationDark">{this.state.data.location}</p>
+				<div className = "infoDark">
 					<div>
 						<p>{this.state.data.public_gists}</p>
 						<p>Gists</p>
